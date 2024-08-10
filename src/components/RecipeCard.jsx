188537280d84b6efbcba39daf58e0f97dce7affc
awafpg <awafpg@gmail.com>
2024-08-10 19:57:31 +0700

    [feature Favorite] menambahkan card

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,35 @@
+import { FaHeart } from "react-icons/fa";
+
+export function RecipeCard() {
+  const imgUrl =
+    "https://peasandcrayons.com/wp-content/uploads/2016/08/Blueberry-Broccoli-Spinach-Salad-Poppyseed-Ranch-dressing-recipe-7143.jpg";
+  return (
+    <card>
+      <div className="bg-white rounded-lg shadow-lg max-w-[280px] p-2">
+        <div className="p-3">
+          <img
+            className=" w-full h-[220px]  object-cover rounded-lg"
+            src={imgUrl}
+            alt="Card Image"
+          />
+        </div>
+        <div className="p-2">
+          <h2 className="text-xl font-bold text-gray-900 mb-4">Salad Sayur</h2>
+          <div>
+            <div className="flex justify-between">
+              <a
+                href="#"
+                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+              >
+                DLihat Resep
+              </a>
+              <button className="bg-white px-8 rounded border border-blue-500 hover:text-red-600 hover:bg-blue-500">
+                <FaHeart className=" text-xl active:text-red-500" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </card>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+import { nextui } from "@nextui-org/react";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/components/(button|snippet|code|input).js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
