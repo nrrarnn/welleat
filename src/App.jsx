3c7c3dde6cd89e6debafa03723f9e841e41dceff
Nur Arini <nurarini0302@gmail.com>
2024-08-11 06:52:27 +0700

    [UPDATE] ubah warna text

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import HomePage from "./pages/homepage/HomePage";
 import DetailRecipe from "./pages/detailRecipePages/DetailRecipe";
 import FavoritePage from "./pages/favoritePage/FavoritePage";
 import UserCard from "./pages/userPage/UserCard";
-import DashboardPage from "./pages/dashboardAdmin/DashboardPage";
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
         <Route element={<DetailRecipe />} path="/detail-recipe" />
         <Route element={<FavoritePage />} path="/favorite" />
         <Route element={<UserCard />} path="/user" />
-        <Route element={<DashboardPage />} path="/dashboard" />
       </Routes>
     </>
   );
