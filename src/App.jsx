810f1ae134ca73e737603f873ef1b95b0822c240
Bachtiar Putera Alamsyah <bachtiaralamsyah10@gmail.com>
2024-08-11 09:45:14 +0700

    [Features] Menambahkan Halaman Login Register

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import HomePage from "./pages/homepage/HomePage";
 import DetailRecipe from "./pages/detailRecipePages/DetailRecipe";
 import FavoritePage from "./pages/favoritePage/FavoritePage";
 import UserCard from "./pages/userPage/UserCard";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
 
 function App() {
   return (
     <>
       <Routes>
         <Route element={<HomePage />} path="/" />
+        <Route element={<Login />} path="/login" />
         <Route element={<DetailRecipe />} path="/detail-recipe" />
         <Route element={<FavoritePage />} path="/favorite" />
         <Route element={<UserCard />} path="/user" />
+        <Route element={<Register />} path="/register" />
       </Routes>
     </>
   );
