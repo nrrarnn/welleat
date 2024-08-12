815ff4c6502f601a9afb97c39f8d814dd8993811
Ilham Maulana <aammaul7@gmail.com>
2024-08-12 08:00:36 +0700

    [arrange] spacing

--- src/pages/DaftarProduct/DaftarProduct.jsx
+++ src/pages/DaftarProduct/DaftarProduct.jsx
@@ -4,40 +4,42 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import RecipeCard from "../favoritePage/RecipeCard";
 
-
 const DaftarProduct = () => {
- 
-  const [listRecipes, setListRecipes] = useState([])
+	const [listRecipes, setListRecipes] = useState([]);
 
-  const getRecipes = async () => {
-    try {
-      const response = await axios.get("https://66b8371e3ce57325ac76a51a.mockapi.io/api/v1/recipelist");
-      setListRecipes(response.data)
-      console.log(response.data)
+	const getRecipes = async () => {
+		try {
+			const response = await axios.get(
+				"https://66b8371e3ce57325ac76a51a.mockapi.io/api/v1/recipelist"
+			);
+			setListRecipes(response.data);
+			console.log(response.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-    }catch(error){
-      console.log(error)
-    }
-  }
+	useEffect(() => {
+		getRecipes();
+	}, []);
 
-  useEffect(() => {
-    getRecipes()
-  },[])
+	return (
+		<>
+			<Header />
 
-  return (
-    <>
-    <Header/>
-
-        <div className="flex flex-wrap gap-3 px-20">
-            {
-              listRecipes.map((recipe,index) => (
-                <RecipeCard key={index} recipeName={recipe.name} image={recipe.image} id={recipe.id} />
-              ))
-            }
-          </div>
-      <Footer/>
-    </>
-  );
+			<div className="flex flex-wrap gap-3 px-20">
+				{listRecipes.map((recipe, index) => (
+					<RecipeCard
+						key={index}
+						recipeName={recipe.name}
+						image={recipe.image}
+						id={recipe.id}
+					/>
+				))}
+			</div>
+			<Footer />
+		</>
+	);
 };
 
 export default DaftarProduct;
